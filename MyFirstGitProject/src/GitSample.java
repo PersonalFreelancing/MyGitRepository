21390242891e648f89729283f15ab97dd98bf8fc
unknown <M1016435@A2ML07734.mindtree.com>
2015-10-01 13:05:56 +1000

    Intial commit

---
+++ MyFirstGitProject/src/GitSample.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author M1016435
+ *
+ */
+public class GitSample {
+
+	/**
+	 * 
+	 */
+	public GitSample() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Git I am uploading for first time");
+
+	}
+
+}
